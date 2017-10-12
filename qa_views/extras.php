7a23157f55558dfe86990cab4bf346030d901430
Stuart Olivera <stuart@stuartolivera.com>
2017-10-11 20:30:32 -0400

    Remove all old content

--- qa_views/extras.php
+++
@@ -1,8 +0,0 @@
-<div class="row">
-	<div class="small-12 columns">
-		<h1>Extras</h1>
-		<p class="large">
-			<b>Our goal is for WIC Hacks to be accessible to women with all levels of programming experience</b>.  With this goal in mind, we have compiled the below list of <b>learn-to-program resources</b>.  Although we want you to arrive at WIC Hacks with a willingness to learn, we also encourage you to check out these resources in advance of the event, particularly if you wish to use a specific language, API, framework, etc. as part of your project.  Happy learning!
-		</p>
-	</div>
-</div>
